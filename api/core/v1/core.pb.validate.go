be5fdb6579a16d6e79d90b31a1a3a5ad5c92708d
Daniel Hess <dan9186@gmail.com>
2021-04-18 14:12:52 -0700

    only support regext matches

--- api/core/v1/core.pb.validate.go
+++ api/core/v1/core.pb.validate.go
@@ -224,24 +224,7 @@ func (m *Listener) Validate() error {
 		}
 	}
 
-	switch m.Match.(type) {
-
-	case *Listener_Triggers:
-
-		if v, ok := interface{}(m.GetTriggers()).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return ListenerValidationError{
-					field:  "Triggers",
-					reason: "embedded message failed validation",
-					cause:  err,
-				}
-			}
-		}
-
-	case *Listener_TriggerRegex:
-		// no validation rules for TriggerRegex
-
-	}
+	// no validation rules for MathcRegex
 
 	return nil
 }
@@ -299,70 +282,6 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = ListenerValidationError{}
-
-// Validate checks the field values on Triggers with the rules defined in the
-// proto definition for this message. If any rules are violated, an error is returned.
-func (m *Triggers) Validate() error {
-	if m == nil {
-		return nil
-	}
-
-	return nil
-}
-
-// TriggersValidationError is the validation error returned by
-// Triggers.Validate if the designated constraints aren't met.
-type TriggersValidationError struct {
-	field  string
-	reason string
-	cause  error
-	key    bool
-}
-
-// Field function returns field value.
-func (e TriggersValidationError) Field() string { return e.field }
-
-// Reason function returns reason value.
-func (e TriggersValidationError) Reason() string { return e.reason }
-
-// Cause function returns cause value.
-func (e TriggersValidationError) Cause() error { return e.cause }
-
-// Key function returns key value.
-func (e TriggersValidationError) Key() bool { return e.key }
-
-// ErrorName returns error name.
-func (e TriggersValidationError) ErrorName() string { return "TriggersValidationError" }
-
-// Error satisfies the builtin error interface
-func (e TriggersValidationError) Error() string {
-	cause := ""
-	if e.cause != nil {
-		cause = fmt.Sprintf(" | caused by: %v", e.cause)
-	}
-
-	key := ""
-	if e.key {
-		key = "key for "
-	}
-
-	return fmt.Sprintf(
-		"invalid %sTriggers.%s: %s%s",
-		key,
-		e.field,
-		e.reason,
-		cause)
-}
-
-var _ error = TriggersValidationError{}
-
-var _ interface {
-	Field() string
-	Reason() string
-	Key() bool
-	Cause() error
-	ErrorName() string
-} = TriggersValidationError{}
 
 // Validate checks the field values on EventMessage with the rules defined in
 // the proto definition for this message. If any rules are violated, an error
